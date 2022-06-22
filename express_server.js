88ad4466fe0497c9dca6089596bb47818d98c849
Hope Warren <hopevaughnwarren@gmail.com>
2022-06-22 12:56:55 -0700

    add random alphanumeric string generator. add body-parser module

--- express_server.js
+++ express_server.js
@@ -2,9 +2,22 @@ const express = require('express');
 const app = express();
 const PORT = 8080; //default port
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
 ///////////////////////////////////////////////////////////////////////////////
 // set the view ingine to ejs
 ///////////////////////////////////////////////////////////////////////////////
+const generateRandomString = () => {
+  let randomString = '';
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (let i = 0; i < 6; i++) {
+    randomString += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return randomString;
+};
+
 
 app.set('view engine', 'ejs');
 
@@ -20,10 +33,19 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   const templateVars = { shortURL: req.params.shortURL, longURL: longURL };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
