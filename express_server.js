c9ddc995f56c64cce9c6104517cd71bfcbf20ae5
Hope Warren <hopevaughnwarren@gmail.com>
2022-06-22 09:43:14 -0700

    add ejs module for server side render

--- express_server.js
+++ express_server.js
@@ -2,22 +2,24 @@ const express = require('express');
 const app = express();
 const PORT = 8080; //default port
 
+///////////////////////////////////////////////////////////////////////////////
+// set the view ingine to ejs
+///////////////////////////////////////////////////////////////////////////////
+
+app.set('view engine', 'ejs');
+
+
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xk": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send('Hello!');
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
 });
 
-app.get('/hello', (req, res) => {
-  res.send(`<html><body>Hello <b>World</b></body></html>`);
-});
-
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port : ${PORT}!`);
